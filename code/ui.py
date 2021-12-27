7f388f398a9562b2ac8d75667c0a4c64bab27198
FalafelFries <63476604+FalafelFries@users.noreply.github.com>
2021-12-27 16:05:09 +0530

    UI added. minor changes tbd

---
+++ code/ui.py
@@ -0,0 +1,63 @@
+from tkinter import *
+from tkinter import filedialog
+import mypack as mp
+
+root=Tk()
+
+def upload():
+    
+    root.filename = filedialog.askopenfilename(initialdir=r"C:\Users\Deenaz\Documents\Python\MiniPro\code\videos", title="Select A File",filetypes=(("mp4 files", "*.mp4"),("all files", "*.*")))
+    print(type(root.filename))
+    mp.foo(root.filename)
+
+def page1(root):
+    page = Frame(root)
+    page.pack()
+    Label(page, text = 'WELCOME', fg='red', font=("Helvetica", 16)).pack(padx=5, pady=10, side="top", fill="both", expand=True)
+    Button(page, text = 'VIDEO', fg='blue', command = changepage1).pack(padx=5, pady=10, side="top", fill="both", expand=True)
+    #Button(page, text = 'IMAGE', fg='blue', command = changepage2).pack(padx=5, pady=10, side="top", fill="both", expand=True)
+
+def page2a(root):
+    page = Frame(root)
+    page.pack()
+    Label(page, text = 'VIDEO', fg='red', font=("Helvetica", 16)).pack(padx=5, pady=10, side="top", fill="both", expand=True)
+    Button(page, text = 'PROCEED', fg='blue', command = upload).pack(padx=5, pady=10, side="top", fill="both", expand=True)
+    Button(page, text = 'BACK', fg='blue', command = changepage1).pack(padx=5, pady=10, side="top", fill="both", expand=True)
+
+#def page2b(root):
+#    page = Frame(root)
+#    page.pack()
+#    Label(page, text = 'IMAGE', fg='red', font=("Helvetica", 16)).pack(padx=5, pady=10, side="top", fill="both", expand=True)
+#    Button(page, text = 'PROCEED', fg='blue', command = upload).pack(padx=5, pady=10, side="top", fill="both", expand=True)
+#    Button(page, text = 'BACK', fg='blue', command = changepage2).pack(padx=5, pady=10, side="top", fill="both", expand=True)
+
+def changepage1():
+    global pagenum, root
+    for widget in root.winfo_children():
+        widget.destroy()
+    if pagenum == 1:
+        page2a(root)
+        pagenum = 2
+    else:
+        page1(root)
+        pagenum = 1
+
+#def changepage2():
+#    global pagenum, root
+#    for widget in root.winfo_children():
+#        widget.destroy()
+#    if pagenum == 1:
+#        page2b(root)
+#        pagenum = 2
+#    else:
+#        page1(root)
+#        pagenum = 1
+
+pagenum = 1
+
+page1(root)
+
+root.title('XXX')
+root.geometry("400x250+450+200")
+
+root.mainloop()
