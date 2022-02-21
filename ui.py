498d691df6cfd5a6d88fb4a05a172a2cafd67286
Deenaz Fathima <fathimadeenaz@gmail.com>
2022-02-21 22:34:44 +0530

    updated UI

--- ui.py
+++ ui.py
@@ -6,10 +6,16 @@ from tkinter import filedialog
 import pandas as pd
 import main
 
+from tkinter import messagebox
+
 root = Tk()
 root.title('Traffic Analysis')
 
 root.geometry("850x480")
+
+root.resizable(False, False)
+
+root.config(bg="#222b4f")
 
 
 def upload():
@@ -30,132 +36,166 @@ def save():
     d1.to_csv(r"C:\Users\Deenaz\Documents\Python\MiniPro\output.csv")
 
 
-def showmsg(page):
-    Label(page, text='File Saved!', fg='blue', font=("Helvetica", 10)).grid(
-        row=18, column=3, columnspan=2)
-
 # def showmsg(page):
-#     Label(page, text='File Saved!', fg='blue', font=("Helvetica", 15)).place(
-#         x=55, y=300)
+#     Label(page, text='File Saved!', fg='blue', font=("Helvetica", 10)).grid(
+#         row=18, column=3, columnspan=2)
+
+# def showmsg(root):
+#     bg = PhotoImage(file=r"C:\Users\Deenaz\Downloads\bg.png")
+#     canvas1 = Canvas(root, width=400, height=400)
+#     canvas1.pack(fill="both", expand=True)
+#     canvas1.create_image(0, 0, image=bg, anchor="nw")
+
+#     canvas1.create_text(425, 30, text="File Saved!",
+#                         fill="pink", font=("Lato", 18))
+#     root.mainloop()
+
+def showmsg():
+    messagebox.showinfo("Notification", "File has been successfully saved")
 
 
-# def page1(root):
-#     page = Frame(root)
-#     page.pack()
-#     Label(page, text='TRAFFIC ANALYSIS', fg='blue', font=("Helvetica", 24)).pack(
-#         padx=5, pady=10, side="top", fill="both", expand=True)
-#     Label(page, text=' ', fg='blue', font=("Helvetica", 10)).pack(
-#         padx=5, pady=10, side="top", fill="both", expand=True)
-#     Label(page, text='WELCOME', fg='red', font=("Helvetica", 16)).pack(
-#         padx=5, pady=10, side="top", fill="both", expand=True)
-#     Button(page, text='BEGIN', fg='blue', command=changepage1).pack(
-#         padx=5, pady=10, side="top", fill="both", expand=True)
+def page1(root):
+    bg = PhotoImage(
+        file=r"C:\Users\Deenaz\Documents\Python\MiniPro\UI\bg.png")
+
+    canvas1 = Canvas(root, width=400, height=400)
+
+    canvas1.pack(fill="both", expand=True)
+
+    canvas1.create_image(0, 0, image=bg, anchor="nw")
+
+    canvas1.create_text(420, 180, text="TRAFFIC ANALYSIS",
+                        fill="#d2d2d7", font=('Helvatica 50 bold'))
+
+    button3 = Button(root, text="       Begin       ", bg="#4467fd",
+                     fg="#d2d2d7", font=("Lato", 20), command=changepage1)
+
+    button3_canvas = canvas1.create_window(
+        325, 270, anchor="nw", window=button3)
+    root.mainloop()
 
 
-# def page2(root):
-#     page = Frame(root)
-#     page.pack()
-#     Label(page, text='VIDEO', fg='red', font=("Helvetica", 16)).pack(
-#         padx=5, pady=10, side="top", fill="both", expand=True)
-#     Button(page, text='UPLOAD FILE', fg='blue', command=upload).pack(
-#         padx=5, pady=10, side="top", fill="both", expand=True)
+def page2(root):
+    bg = PhotoImage(file=r"C:\Users\Deenaz\Documents\Python\MiniPro\UI\bg.png")
+
+    canvas1 = Canvas(root, width=400, height=400)
+
+    canvas1.pack(fill="both", expand=True)
+
+    canvas1.create_image(0, 0, image=bg,
+                         anchor="nw")
+
+    canvas1.create_text(420, 100, text="INPUT VIDEO",
+                        fill="#d2d2d7", font=('Helvatica 35'))
+
+    canvas1.create_text(420, 221, text="Select file to be uploaded",
+                        fill="#d2d2d7", font=("Lato", 20))
+
+    canvas1.create_text(420, 254, text="File uploaded must be in mp4 format",
+                        fill="#d2d2d7", font=("Lato", 20))
+
+    button3 = Button(root, text="       Browse       ", bg="#4467fd",
+                     fg="#d2d2d7", font=("Lato", 20), command=upload)
+
+    button3_canvas = canvas1.create_window(
+        320, 350, anchor="nw",  window=button3)
+    root.mainloop()
 
 
 # def page3(root):
 #     page = Frame(root)
 #     page.pack()
-#     Label(page, text='RESULTS', fg='blue').grid(row=1, column=3, columnspan=2)
-#     Label(page, text='   ', fg='red', font=("Helvetica", 8)).grid(
+#     Label(page, text='RESULTS', fg='blue', font=(
+#         "Helvetica", 25)).grid(row=1, column=3, columnspan=2)
+#     Label(page, text='   ', fg='red', font=("Helvetica", 5)).grid(
 #         row=2, column=3, columnspan=2)
-#     Label(page, text='Car', fg='red').grid(row=4, column=2)
-#     Label(page, text='Motorbike', fg='red').grid(row=5, column=2)
-#     Label(page, text='Bus', fg='red').grid(row=6, column=2)
-#     Label(page, text='Truck', fg='red').grid(row=7, column=2)
-#     Label(page, text='Upwards', fg='red').grid(row=3, column=4)
-#     Label(page, text=res[0], fg='red').grid(row=4, column=4)
-#     Label(page, text=res[1], fg='red').grid(row=5, column=4)
-#     Label(page, text=res[2], fg='red').grid(row=6, column=4)
-#     Label(page, text=res[3], fg='red').grid(row=7, column=4)
-#     Label(page, text='Downwards', fg='red').grid(row=3, column=5)
-#     Label(page, text=res[4], fg='red').grid(row=4, column=5)
-#     Label(page, text=res[5], fg='red').grid(row=5, column=5)
-#     Label(page, text=res[6], fg='red').grid(row=6, column=5)
-#     Label(page, text=res[7], fg='red').grid(row=7, column=5)
+#     Label(page, text='Car', fg='red', font=(
+#         "Helvetica", 18)).grid(row=4, column=2)
+#     Label(page, text='Motorbike', fg='red', font=(
+#         "Helvetica", 18)).grid(row=5, column=2)
+#     Label(page, text='Bus', fg='red', font=(
+#         "Helvetica", 18)).grid(row=6, column=2)
+#     Label(page, text='Van', fg='red', font=(
+#         "Helvetica", 18)).grid(row=7, column=2)
+#     Label(page, text='Upwards', fg='red', font=(
+#         "Helvetica", 18)).grid(row=3, column=4)
+#     Label(page, text=res[0], fg='red', font=(
+#         "Helvetica", 18)).grid(row=4, column=4)
+#     Label(page, text=res[1], fg='red', font=(
+#         "Helvetica", 18)).grid(row=5, column=4)
+#     Label(page, text=res[2], fg='red', font=(
+#         "Helvetica", 18)).grid(row=6, column=4)
+#     Label(page, text=res[3], fg='red', font=(
+#         "Helvetica", 18)).grid(row=7, column=4)
+#     Label(page, text='Downwards', fg='red', font=(
+#         "Helvetica", 18)).grid(row=3, column=5)
+#     Label(page, text=res[4], fg='red', font=(
+#         "Helvetica", 18)).grid(row=4, column=5)
+#     Label(page, text=res[5], fg='red', font=(
+#         "Helvetica", 18)).grid(row=5, column=5)
+#     Label(page, text=res[6], fg='red', font=(
+#         "Helvetica", 18)).grid(row=6, column=5)
+#     Label(page, text=res[7], fg='red', font=(
+#         "Helvetica", 18)).grid(row=7, column=5)
+#     # Button(page, text='SAVE', fg='blue', font=("Helvetica", 18), command=lambda: [save(), showmsg(page)]).grid(
+#     #     row=14, column=2)
 #     Button(page, text='SAVE', fg='blue', command=lambda: [save(), showmsg(page)]).grid(
 #         row=17, column=3, columnspan=2)
 
-
-def page1(root):
-    # bg = PhotoImage(
-    #     file=r"C:\Users\Deenaz\Downloads\output-onlinepngtools.png")
+def page3(root):
+    bg = PhotoImage(file=r"C:\Users\Deenaz\Documents\Python\MiniPro\UI\bg.png")
 
     canvas1 = Canvas(root, width=400,
                      height=400)
 
     canvas1.pack(fill="both", expand=True)
 
-    # canvas1.create_image(0, 0, image=bg,
-    #                      anchor="nw")
+    canvas1.create_image(0, 0, image=bg,
+                         anchor="nw")
 
-    canvas1.create_text(425, 150, text="TRAFFIC ANALYSIS",
-                        fill="red", font=('Helvetica 50 bold'))
+    canvas1.create_text(420, 100, text="RESULTS",
+                        fill="#d2d2d7", font=('Helvatica 35'))
 
-    button3 = Button(root, text="       Begin       ",
-                     fg='blue', font=("Helvetica", 20), command=changepage1)
+    canvas1.create_text(220, 200, text="Car",
+                        fill="#d2d2d7", font=("Lato", 18))
+    canvas1.create_text(220, 235, text="Motorbike",
+                        fill="#d2d2d7", font=("Lato", 18))
+    canvas1.create_text(220, 270, text="Bus",
+                        fill="#d2d2d7", font=("Lato", 18))
+    canvas1.create_text(220, 305, text="Van",
+                        fill="#d2d2d7", font=("Lato", 18))
 
-    button3_canvas = canvas1.create_window(325, 270, anchor="nw",
-                                           window=button3)
+    canvas1.create_text(420, 160, text="Upwards",
+                        fill="#d2d2d7", font=("Lato", 18))
+
+    canvas1.create_text(420, 200, text=res[0],
+                        fill="#d2d2d7", font=("Lato", 18))
+    canvas1.create_text(420, 235, text=res[1],
+                        fill="#d2d2d7", font=("Lato", 18))
+    canvas1.create_text(420, 270, text=res[2],
+                        fill="#d2d2d7", font=("Lato", 18))
+    canvas1.create_text(420, 305, text=res[3],
+                        fill="#d2d2d7", font=("Lato", 18))
+
+    canvas1.create_text(600, 160, text="Downwards",
+                        fill="#d2d2d7", font=("Lato", 18))
+
+    canvas1.create_text(600, 200, text=res[4],
+                        fill="#d2d2d7", font=("Lato", 18))
+    canvas1.create_text(600, 235, text=res[5],
+                        fill="#d2d2d7", font=("Lato", 18))
+    canvas1.create_text(600, 270, text=res[6],
+                        fill="#d2d2d7", font=("Lato", 18))
+    canvas1.create_text(600, 305, text=res[7],
+                        fill="#d2d2d7", font=("Lato", 18))
+
+    button3 = Button(root, text="       SAVE       ", bg="#4467fd", fg="#d2d2d7", font=(
+        "Lato", 20), command=lambda: [save(), showmsg()])
+
+    button3_canvas = canvas1.create_window(
+        330, 350, anchor="nw", window=button3)
+
     root.mainloop()
-
-
-def page2(root):
-    page = Frame(root)
-    page.pack()
-    Label(page, text='VIDEO', fg='red', font=("Helvetica", 25)).pack(
-        padx=5, pady=100, side="top", fill="both", expand=True)
-    Button(page, text='UPLOAD FILE', fg='blue', font=("Helvetica", 15), command=upload).pack(
-        padx=5, pady=100, side="top", fill="both", expand=True)
-
-
-def page3(root):
-    page = Frame(root)
-    page.pack()
-    Label(page, text='RESULTS', fg='blue', font=(
-        "Helvetica", 25)).grid(row=1, column=3, columnspan=2)
-    Label(page, text='   ', fg='red', font=("Helvetica", 5)).grid(
-        row=2, column=3, columnspan=2)
-    Label(page, text='Car', fg='red', font=(
-        "Helvetica", 18)).grid(row=4, column=2)
-    Label(page, text='Motorbike', fg='red', font=(
-        "Helvetica", 18)).grid(row=5, column=2)
-    Label(page, text='Bus', fg='red', font=(
-        "Helvetica", 18)).grid(row=6, column=2)
-    Label(page, text='Truck', fg='red', font=(
-        "Helvetica", 18)).grid(row=7, column=2)
-    Label(page, text='Upwards', fg='red', font=(
-        "Helvetica", 18)).grid(row=3, column=4)
-    Label(page, text=res[0], fg='red', font=(
-        "Helvetica", 18)).grid(row=4, column=4)
-    Label(page, text=res[1], fg='red', font=(
-        "Helvetica", 18)).grid(row=5, column=4)
-    Label(page, text=res[2], fg='red', font=(
-        "Helvetica", 18)).grid(row=6, column=4)
-    Label(page, text=res[3], fg='red', font=(
-        "Helvetica", 18)).grid(row=7, column=4)
-    Label(page, text='Downwards', fg='red', font=(
-        "Helvetica", 18)).grid(row=3, column=5)
-    Label(page, text=res[4], fg='red', font=(
-        "Helvetica", 18)).grid(row=4, column=5)
-    Label(page, text=res[5], fg='red', font=(
-        "Helvetica", 18)).grid(row=5, column=5)
-    Label(page, text=res[6], fg='red', font=(
-        "Helvetica", 18)).grid(row=6, column=5)
-    Label(page, text=res[7], fg='red', font=(
-        "Helvetica", 18)).grid(row=7, column=5)
-    # Button(page, text='SAVE', fg='blue', font=("Helvetica", 18), command=lambda: [save(), showmsg(page)]).grid(
-    #     row=14, column=2)
-    Button(page, text='SAVE', fg='blue', command=lambda: [save(), showmsg(page)]).grid(
-        row=17, column=3, columnspan=2)
 
 
 def changepage1():
@@ -175,9 +215,5 @@ def changepage2():
 pagenum = 1
 
 page1(root)
-
-# root.title('Traffic Analysis')
-# root.geometry("400x250+450+200")
-
 
 root.mainloop()
